479b08da8e4cb5e1738130fb66544ca27c8442dd
Francesca M <francesca.madeddu@flexys.co.uk>
2022-08-16 13:47:55 +0100

    Another draft

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,22 @@ export default function Home() {
         <link rel="icon" href="/icons/wedding-day-48.png" />
       </Head>
 
-      <p className="caption">
-        <a href="https://paypal.me/moodymood">Paypal Link</a>
-      </p>
+      <div className="caption">
+        <div class="title">
+          <span class="title__names">Lolla & Salvatore</span>
+          <span class="title__date">01 10 2022</span>
+        </div>
+        <div class="main">
+          <div>
+            Ciao Amici,
+            <br />
+            siamo felici di condividere con voi la nostra gioia.
+          </div>
+          <div>Non abbiamo una lista nozze, ma se vi va potete mandarci da qualche parte in giro per il mondo.</div>
+        </div>
+        <div class="footer"><a href="https://paypal.me/moodymood">Paypal Link</a></div>
+
+      </div>
 
     </div>
   )
