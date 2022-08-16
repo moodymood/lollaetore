5d819153a03d2fcc667943102e1fb29c08cef950
Francesca M <francesca.madeddu@flexys.co.uk>
2022-08-16 14:05:02 +0100

    Paypal link

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
           </div>
           <div>Non abbiamo una lista nozze, ma se vi va potete mandarci da qualche parte in giro per il mondo.</div>
         </div>
-        <div class="footer"><a href="https://paypal.me/moodymood">Paypal Link</a></div>
+        <div class="footer"><a href="https://paypal.me/lollaetore">Paypal Link</a></div>
 
       </div>
 
